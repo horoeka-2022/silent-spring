d2b61279f5fca05983ab729db448348ca2f853b0
ciara <cbdoelman@gmail.com>
2022-12-01 17:36:50 +1300

    merge with main and add more dots

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -16,6 +16,17 @@ function App() {
           <Box position={[1.2, 0, 0]} />
           <Box position={[3.6, 2.4, 0]} />
           <Box position={[5.9, 6.0, 1.0]} />
+          <Box position={[5.9, 6.0, 1.0]} />
+          <Box position={[-4, -2, 0]} />
+          <Box position={[8, 5, 3]} />
+          <Box position={[3, -5, 3]} />
+          <Box position={[-5, -3, 6]} />
+          <Box position={[10, 6.0, 10]} />
+          <Box position={[7, 7, 7]} />
+          <Box position={[1, 3, 5]} />
+          <Box position={[2, -5, 6]} />
+          <Box position={[0, 6.0, -7]} />
+          <Box position={[5, 10, -5]} />
           <OrbitControls />
         </Canvas>
       </main>
