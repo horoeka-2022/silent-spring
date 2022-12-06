da0ca37eaf54469bd4392dc474bc3dd88bde6567
ciara <cbdoelman@gmail.com>
2022-12-07 10:44:02 +1300

    make heart shape

--- client/components/Bird.jsx
+++ client/components/Bird.jsx
@@ -2,9 +2,11 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
 import { a, useSpring } from '@react-spring/three'
+import * as THREE from 'three'
 
 import Sound from './Sound'
 import Info from './Info'
+import { DoubleSide } from 'three'
 
 function Bird({ position, data: birdData }) {
   const bird = useRef(null)
@@ -19,7 +21,7 @@ function Bird({ position, data: birdData }) {
     active: 0,
     clicked: false,
     visible: true,
-    phase: 5
+    phase: 5,
   })
 
   // conditionally render animation and info
@@ -40,8 +42,13 @@ function Bird({ position, data: birdData }) {
     const handleRemove = (event) => {
       if (event.key === ' ') {
         setBirdState({ ...birdState, phase: birdState.phase - 1 })
-        if (birdState.visible && (birdData.Status === birdState.phase)) {
-          setBirdState({ ...birdState, active: 0, clicked: false, visible: false })
+        if (birdState.visible && birdData.Status === birdState.phase) {
+          setBirdState({
+            ...birdState,
+            active: 0,
+            clicked: false,
+            visible: false,
+          })
         }
       }
     }
@@ -74,6 +81,25 @@ function Bird({ position, data: birdData }) {
   const scale = spring.to([0, 1], [1, 5])
   const rotation = spring.to([0, 1], [0, Math.PI])
 
+  //shape setup
+
+  function getHeartShape() {
+    const x = 5,
+      y = 5
+
+    const heartShape = new THREE.Shape()
+
+    heartShape.moveTo(x + 5, y + 5)
+    heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y)
+    heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7)
+    heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19)
+    heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7)
+    heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y)
+    heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5)
+
+    return heartShape
+  }
+
   return (
     <a.mesh
       ref={bird}
@@ -84,11 +110,11 @@ function Bird({ position, data: birdData }) {
       onClick={handleClick}
       visible={birdState.visible ? true : false}
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"hotpink"} />
+      <shapeGeometry args={[getHeartShape()]} />
+      <meshStandardMaterial color={'hotpink'} side={DoubleSide} />
       <Sound url={audioUrl} visible={birdState.visible} vol={1} />
       {/* Not using && because when false, returns a non-null value */}
-      {(birdState.clicked && birdState.visible) ? <Info data={birdData} /> : null}
+      {birdState.clicked && birdState.visible ? <Info data={birdData} /> : null}
     </a.mesh>
   )
 }
